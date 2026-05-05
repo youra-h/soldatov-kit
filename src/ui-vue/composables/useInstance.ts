cde060fb3da8824b26604f505186eb7e96ecc332
Yuri Soldatov <bestletter@inbox.ru>
2026-05-05 23:46:24 +0500

    feat: обновить типизацию параметров функции useInstance для улучшения управления экземплярами компонентов

--- src/ui-vue/composables/useInstance.ts
+++ src/ui-vue/composables/useInstance.ts
@@ -1,9 +1,15 @@
 import { reactive, toRaw, type Reactive } from 'vue'
 import type { IComponentModelOptions } from '@core'
 
+/**
+ * Use a managed instance of a component.
+ * @param Ctor The constructor of the component.
+ * @param props The props to pass to the component.
+ * @returns The managed instance of the component.
+ */
 export function useInstance<T extends object>(
 	Ctor: new (options: IComponentModelOptions<any>) => T,
-	props: { ctrl?: T | Reactive<T>; [key: string]: any },
+	props: any,
 ): Reactive<T> {
 	const provided = props.ctrl
 
