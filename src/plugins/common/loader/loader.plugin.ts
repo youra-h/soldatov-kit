25216d92b496f6557e3572ce6a885eb9d0924760
Yuri Soldatov <bestletter@inbox.ru>
2026-06-10 22:26:44 +0500

    feat: улучшить управление состоянием загрузчика и добавить резольвер для состояния disabled

--- src/plugins/common/loader/loader.plugin.ts
+++ src/plugins/common/loader/loader.plugin.ts
@@ -5,10 +5,16 @@ import type { IPluginBundle } from '../../base/types'
 
 export type TLoaderPluginEvents = {}
 
+/**
+ * Плагин для управления состоянием загрузки в компонентах. Позволяет связать состояние загрузчика (ILoader) с контролами, обеспечивая автоматическое управление состоянием disabled у контролов на основе состояния загрузчика.
+ * Если загрузчик активен (disabled: true), то связанные контролы также будут дизейблены. Если загрузчик неактивен (disabled: false), то контролы будут возвращаться в свое обычное состояние (зависит от их собственного состояния).
+ * Это обеспечивает удобство использования и согласованность интерфейса, позволяя автоматически управлять состоянием контролов на основе состояния загрузчика.
+ */
 export class TLoaderPlugin extends TBasePlugin<TLoaderPluginEvents> {
 	static readonly key = 'loader'
 
 	private _loader?: ILoader
+	private _bundle?: IPluginBundle
 
 	get loader(): ILoader | undefined {
 		return this._loader
@@ -16,27 +22,39 @@ export class TLoaderPlugin extends TBasePlugin<TLoaderPluginEvents> {
 
 	set loader(value: ILoader) {
 		this._loader = value
+		this._link()
 	}
 
 	override install(bundle: IPluginBundle): void {
-		const instancePlugin = bundle.get(TInstancePlugin) as TInstancePlugin<IControl> | undefined
-
-		if (!instancePlugin) return
-
-		const disableState = instancePlugin.instance!.states.disabled
-
-		if (disableState && this._loader) {
-			const loader = this._loader
-			disableState.setResolver((value) => value || loader.disabled)
-		}
+		this._bundle = bundle
+		this._link()
 	}
 
-	private _setResolver(): void {
-		
+	/**
+	 * Связывает состояние disabled у индикатора загрузки и контролов, если оба плагина установлены.
+	 * Если загрузчик активен (disabled: true), то связанные контролы также будут дизейблены.
+	 * Если загрузчик неактивен (disabled: false), то контролы будут возвращаться в свое обычное состояние (зависит от их собственного состояния).
+	 * Это позволяет автоматически управлять состоянием контролов на основе состояния загрузчика, обеспечивая удобство использования и согласованность интерфейса.
+	 */
+	private _link(): void {
+		if (!this._loader || !this._bundle) return
+
+		const instancePlugin = this._bundle.get(TInstancePlugin) as
+			| TInstancePlugin<IControl>
+			| undefined
+
+		if (!instancePlugin?.instance) return
+
+		const disableState = instancePlugin.instance.states.disabled
+		const loader = this._loader
+
+		// Устанавливаем резольвер для состояния disabled, который будет возвращать true, если загрузчик активен (disabled: true).
+		disableState.setResolver((value) => value || loader.disabled)
 	}
 
 	destroy(): void {
 		this._loader = undefined
+		this._bundle = undefined
 		super.destroy()
 	}
 }
