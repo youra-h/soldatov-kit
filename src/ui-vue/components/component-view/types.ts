260eabb9cdddbdc38e23799cfbff77a061fe3128
Youra <bestletter@inbox.ru>
2026-07-07 21:34:46 +0500

    feat: добавить поддержку плагинов в типы TBaseComponentViewProps и TBaseComponentProps

--- src/ui-vue/components/component-view/types.ts
+++ src/ui-vue/components/component-view/types.ts
@@ -1,10 +1,7 @@
 import type { IComponentView } from '@core'
-import type { IPluginBundle } from '@plugins'
 import type { TBaseComponentProps } from '../component'
 
 export type TBaseComponentViewProps<
 	TCoreProps,
 	TInstance extends IComponentView = IComponentView,
-> = TBaseComponentProps<TCoreProps, TInstance> & {
-	plugins?: IPluginBundle | undefined
-}
+	> = TBaseComponentProps<TCoreProps, TInstance> & {}
