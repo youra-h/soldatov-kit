f7b3c1a9c9ba8dc95e6420865754d394ba95df2c
Youra <bestletter@inbox.ru>
2026-07-08 20:52:46 +0500

    update paths

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,36 +1,11 @@
 // base (слои/ядро)
-export * from './base/entity'
-export * from './base/component'
-export * from './base/component-view'
-export * from './base/interactive'
-export * from './base/stylable'
-export * from './base/control'
-export * from './base/textable'
-export * from './base/value-control'
-export * from './base/input-control'
+export * from './components/base'
 export * from './common/state-unit'
 export * from './common/states'
 export * from './common/size'
-export * from './base/collection'
-
-// behaviors (пока используются деревом)
-export * from './base/behaviors'
 
 // components (headless компоненты)
-export * from './components/icon'
-export * from './components/spinner'
-export * from './components/skeleton'
-export * from './components/button'
-export * from './components/check-box'
-export * from './components/switch'
-export * from './components/input'
-export * from './components/tabs'
-export * from './components/tree'
-export * from './components/collapse'
-export * from './components/list'
-export * from './components/list-box'
-export * from './components/drag-and-drop'
-export * from './components/frame'
+export * from './components/custom'
 
 // common (вспомогательные утилиты)
 export * from './common/is-same'
