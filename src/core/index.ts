1b597799fc63f151df76080ed95fb8f7f6e4044a
Youra <bestletter@inbox.ru>
2026-07-08 21:23:15 +0500

    update path

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,4 @@
-// base (слои/ядро)
-export * from './components/base'
-export * from './common/state-unit'
-export * from './common/states'
-
 // components (headless компоненты)
-export * from './components/custom'
-
-// common (вспомогательные утилиты)
-export * from './common/utility'
-export * from './common/event/evented'
-export * from './common/event/event-emitter'
-export * from './common/classes'
-export * from './bridge/frame-debounce'
+export * from './components'
+// вспомогательные функции
+export * from './bridge'
